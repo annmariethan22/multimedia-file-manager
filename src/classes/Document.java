d8384f696cb9ea80137a7155f3b7966bdec49d32
AnnMarie <annmarie@example.com>
2026-02-20 19:32:33 +0200

    ντοκιουμεντ

--- src/classes/Document.java
+++ src/classes/Document.java
@@ -1,4 +1,3 @@
-
 package classes;
 
 import java.util.ArrayList;
@@ -12,6 +11,10 @@ public class Document {
     private List<String> versions;
     private int currentVersionNumber;
 
+    
+    public Document() {} 
+
+    
     public Document(String title, String authorName, String category, String creationDate, String firstContent) {
         this.title = title;
         this.authorName = authorName;
@@ -23,38 +26,38 @@ public class Document {
     }
 
     public void updateContent(String newContent) {
-    this.versions.add(newContent); 
-    this.currentVersionNumber++;  
-}
+        this.versions.add(newContent); 
+        this.currentVersionNumber++;  
+    }
 
     
-
-    // απλοι χρήστες 
     public String getVisibleContentForSimpleUser() {
         return versions.get(versions.size() - 1);
     }
 
-    // αντιμινς 
+    // Authors 
     public List<String> getVisibleVersionsForAuthor() {
         int total = versions.size();
         if (total <= 3) {
             return new ArrayList<>(versions);
         } else {
-            // 3 τελευταίες εκδόσεις
             return new ArrayList<>(versions.subList(total - 3, total));
         }
     }
-    public Document() {} 
+    
     
     public String getTitle() { return title; }
     public String getAuthorName() { return authorName; }
     public String getCategory() { return category; }
+    public String getCreationDate() { return creationDate; }
+    public List<String> getVersions() { return versions; }
     public int getCurrentVersionNumber() { return currentVersionNumber; }
-}
 
-public void setTitle(String title) { this.title = title; }
-public void setAuthorName(String authorName) { this.authorName = authorName; }
-public void setCategory(String category) { this.category = category; }
-public void setCreationDate(String creationDate) { this.creationDate = creationDate; }
-public void setVersions(List<String> versions) { this.versions = versions; }
-public void setCurrentVersionNumber(int currentVersionNumber) { this.currentVersionNumber = currentVersionNumber; }
+    
+    public void setTitle(String title) { this.title = title; }
+    public void setAuthorName(String authorName) { this.authorName = authorName; }
+    public void setCategory(String category) { this.category = category; }
+    public void setCreationDate(String creationDate) { this.creationDate = creationDate; }
+    public void setVersions(List<String> versions) { this.versions = versions; }
+    public void setCurrentVersionNumber(int currentVersionNumber) { this.currentVersionNumber = currentVersionNumber; }
+}
